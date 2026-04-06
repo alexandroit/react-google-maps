85978b86069fa0b3ed74c30c49d6591b633b4dae
Alexandro Paixao Marques <aleinfoit@gmail.com>
2026-04-05 21:51:29 -0400

    chore(react-19): publish patch release 19.0.1

--- docs-src/react-19/src/main.tsx
+++ docs-src/react-19/src/main.tsx
@@ -5,6 +5,6 @@ import './app.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App reactLine="React 19" reactVersion="19.2.4" docsPath="react-19" packageVersion="19.0.0" />
+    <App reactLine="React 19" reactVersion="19.2.4" docsPath="react-19" packageVersion="19.0.1" />
   </React.StrictMode>
 );
